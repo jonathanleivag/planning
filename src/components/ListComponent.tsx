9cece14eeb9b59baa60985e366189a2542be8f44
jonathanleivag <email@jonathanleivag.cl>
2022-09-18 00:14:20 -0300

    scroll en la lista

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -18,7 +18,7 @@ const ListComponent: FC<ListComponentProps> = ({
       data-list={name}
       onDrop={handleOnDrop}
       onDragOver={event => event.preventDefault()}
-      className='bg-gray-500 flex flex-col flex-shrink-0 md:flex-shrink w-full md:w-[33%] items-center my-3 pb-5 rounded-lg'
+      className='bg-gray-500 flex flex-col flex-shrink-0 md:flex-shrink w-full md:w-[33%] items-center my-3 rounded-lg max-h-screen overflow-y-scroll pb-20'
     >
       <h2 className='w-full px-5 py-5 font-bold'>{name}: </h2>
       {children}
